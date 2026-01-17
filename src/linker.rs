33ee41550b7587847560460a56867dced0387216
djole <djolertrk@gmail.com>
2026-01-17 09:18:29 +0100

    chore: use local ../miden-vm path dependencies
    Update Cargo.toml to use local miden-vm v0.19.1 from
    feature/debug-variable-locations-v0.19.1 branch.

--- src/linker.rs
+++ src/linker.rs
@@ -8,7 +8,7 @@ use std::{
 
 use miden_assembly::SourceManager;
 use miden_assembly_syntax::{
-    Library, PathBuf as LibraryPathBuf,
+    Library,
     diagnostics::{IntoDiagnostic, Report},
 };
 
@@ -90,11 +90,8 @@ impl LinkLibrary {
     ) -> Result<Arc<Library>, Report> {
         match self.kind {
             LibraryKind::Masm => {
-                let namespace = LibraryPathBuf::new(&self.name).map_err(|err| {
-                    Report::msg(format!("invalid library namespace '{}': {err}", &self.name))
-                })?;
                 miden_assembly::Assembler::new(source_manager)
-                    .assemble_library_from_dir(path, &namespace)
+                    .assemble_library_from_dir(path, self.name.as_ref())
                     .map(Arc::new)
             }
             LibraryKind::Masp => {
